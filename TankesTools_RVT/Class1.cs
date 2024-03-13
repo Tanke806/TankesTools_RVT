83c5574be7aecda1bef8d261bd1c7f0b82042cad
Tanke806 <104646742+Tanke806@users.noreply.github.com>
2024-03-13 16:29:28 +0100

    Initial Update of VS Project
    Just copy what is in the course

---
+++ TankesTools_RVT/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TankesTools_RVT
+{
+    public class Class1
+    {
+    }
+}
