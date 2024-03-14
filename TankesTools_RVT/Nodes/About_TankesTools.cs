7a3cc74d71f2f8510f63c081820d5e59c0d47b44
Tanke806 <104646742+Tanke806@users.noreply.github.com>
2024-03-14 10:14:35 +0100

    Adding C# documentation

--- TankesTools_RVT/Nodes/About_TankesTools.cs
+++ TankesTools_RVT/Nodes/About_TankesTools.cs
@@ -8,10 +8,12 @@ namespace TankesTools_RVT
 {
     public class TankesTools
     {
+        // private hides the class in the search browser of dynamo
        private TankesTools() { }
        public static string About()
         {
-            return "This is The firs node of Tankes Tools package";
+            // return is the fuction that control the output but is not the visual name
+            return "This is The firs test node of Tankes Tools package";
         }
     }
 }
