3015eb64cdcd5a7342954fc6acfff98c82006b52
Tanke806 <104646742+Tanke806@users.noreply.github.com>
2024-03-15 14:35:27 +0100

    New node "GetAllPlacedViews" that counts all placed views in input Sheet

---
+++ TankesTools_RVT/Revit/Views/Sheet.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Autodesk.Revit.DB;
+using Revit.Elements.Views;
+
+namespace TankesTools_RVT.Revit.Views.Sheets
+{
+    /// <summary>
+    /// Wraper class for sheets
+    /// </summary>
+    public class Sheet
+    {
+        private Sheet() { }
+        /// <summary>
+        /// Count all placed views on the input sheet
+        /// </summary>
+        /// <param name="sheet"></param>
+        /// <returns></returns>
+        public static int GetAllPlacedViews(global::Revit.Elements.Views.Sheet sheet)
+        {
+            //This cast the Dynamo Revit sheet to a Revit.DB Viewsheet
+            ViewSheet viewSheet = sheet.InternalElement as ViewSheet;
+
+            var placeViews = viewSheet.GetAllPlacedViews();
+
+            return placeViews.Count;
+        }
+    }
+}
